4f994f6e459f9bf382c16e03ca53864659f36e7e
Leslie Fleming <leslie_fleming@elbicon.com>
2021-10-20 19:19:43 +0100

    0.0.2 reading from bucket flat yaml file

--- com.sap.mcdaas.iel/src/main/java/IELDataShift.java
+++ com.sap.mcdaas.iel/src/main/java/IELDataShift.java
@@ -1,9 +1,14 @@
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 
-import java.io.File;
 import java.io.IOException;
 
 public class IELDataShift implements RequestHandler<Object, Object> {
@@ -18,10 +23,40 @@ public class IELDataShift implements RequestHandler<Object, Object> {
     }
 
     private static void getAwsKey() throws IOException {
-        File awsConfiguration = new File("awsConfiguration.yaml");
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        AwsKeys awsKeys = mapper.readValue(awsConfiguration, AwsKeys.class);
-        System.out.println("My-Access-Key=" + awsKeys.getAccess());
-        System.out.println("My-Secret-Key=" + awsKeys.getSecret());
+        final String s3Bucket = "com.sap.mcdaas.iel.dev";
+        //final String objectKey = "configuration/configuration.yaml";
+        final String objectKey = "configuration/test.yaml";
+
+        // Local Test File
+        //File awsConfiguration = new File("awsConfiguration.yaml");
+
+        // yaml mapper
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+
+        // dev region
+        final AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+                                                 .withRegion(Regions.EU_WEST_1)
+                                                 .build();
+
+        //AWS S3 Bucket File
+        try {
+            S3Object s3Object =  amazonS3.getObject(new GetObjectRequest(s3Bucket,objectKey));
+
+            Hyperscalers hyperScalers = objectMapper.readValue(s3Object.getObjectContent(), Hyperscalers.class);
+
+            System.out.println("Hyperscalers=" + hyperScalers.getApi());
+        } catch(AmazonServiceException e){
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+
+
+
+
+
+
+        //AwsKeys awsKeys = mapper.readValue(awsConfiguration, AwsKeys.class);
+        //System.out.println("My-Access-Key=" + awsKeys.getAccess());
+        //System.out.println("My-Secret-Key=" + awsKeys.getSecret());
     }
 }
