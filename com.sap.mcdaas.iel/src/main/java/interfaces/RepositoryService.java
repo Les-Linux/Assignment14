7e1e1d6a5b266b97c92eb8599af7335613b132b4
Les <lfleming@elbicon.com>
2022-03-22 14:53:27 +0000

    delete dir confirmation

--- com.sap.mcdaas.iel/src/main/java/interfaces/RepositoryService.java
+++
@@ -1,6 +0,0 @@
-package interfaces;
-
-public interface RepositoryService {
-   Boolean save(String project, String dateTimeZone, HyperscalerConfiguration configuration,String region);
-   Boolean save(String project, StringBuilder data, String region, String bucket, String objectName);
-}
